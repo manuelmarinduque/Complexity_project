931f8b2ae5516930a79d6484afdf549d80292842
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2020-09-21 12:22:25 -0500

    El presupuesto se cambia a millones

--- regions/classes/example_pulp.py
+++ regions/classes/example_pulp.py
@@ -11,7 +11,7 @@ x7 = LpVariable('x7', lowBound=0, cat='Integer')
 prob += 2*x1 + 5*x2 + 1*x3 + 4*x4 + 3*x5 + 6*x6 + 7*x7
 prob += x1 + x2 + 2*x3 + 2*x4 + x5 + x6 + x7 <= 1000
 prob += 2*x1 + 4*x2 + 1*x3 + 1*x4 + 1*x5 + 3*x6 + 2*x7 <= 2000
-prob += 5*x1 + 4*x2 + 4*x3 + 1.5*x4 + 5*x5 + 3.5*x6 + 4.5*x7 <= 3500
+prob += 5000000*x1 + 4000000*x2 + 4000000*x3 + 1500000*x4 + 5000000*x5 + 3500000*x6 + 4500000*x7 <= 3500000000
 prob += 4*x2 + 2*x5 + 3*x7 >= 1000
 print(LpStatus[prob.status])
 prob.solve()
