084f460157324830cd4febf56f030135b4b44626
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2020-09-21 21:07:27 -0500

    Modelo realizado

--- regions/classes/generar_tabla.py
+++ regions/classes/generar_tabla.py
@@ -1,43 +1,43 @@
 import pandas as pd
-# import gekko as gk
+from pulp import LpProblem, LpMaximize, LpVariable, LpStatus, value
 
 class LinearProgramming():
     
     def __init__(self, num_regiones):
-        self.num_regiones = num_regiones
+        self.__num_regiones = num_regiones
 
-    def generarTabla(self):
-        info = [[50, 1000, 2, 5000, 550], 
-                [25, 100, 4, 400, 350], 
-                [90, 3000, 1, 4000, 2600],
-                [40, 500, 1, 1500, 1000],
-                [35, 700, 1, 5000, 300],
-                [22, 80, 3, 3500, 250],
-                [30, 60, 2, 4500, 300]]
-        df = pd.DataFrame(info, columns=('Población', 'Estaciones', 'Personal', 'Costos', 'Muertes'))
+    def __construirTabla(self):
+        info = [[50, 1000, 2, 5000000, 550, 0], 
+                [25, 100, 4, 4000000, 350, 4], 
+                [90, 3000, 1, 4000000, 2600, 0],
+                [40, 500, 1, 1500000, 1000, 0],
+                [35, 700, 1, 5000000, 300, 2],
+                [22, 80, 3, 3500000, 250, 0],
+                [30, 60, 2, 4500000, 300, 3]]
+        df = pd.DataFrame(info, columns=('Población', 'Estaciones', 'Personal', 'Costos', 'Muertes', 'Cualificación'))
         return df
 
-    def estacionesPorHabitantes(self, tabla):
+    def __estacionesPorHabitantes(self, tabla):
         estaciones = tabla['Estaciones']
         poblacion = tabla['Población']
         resultado = estaciones.div(poblacion)
         tabla['est*hab'] = resultado
         return tabla
 
-    def muertesPorMillon(self, tabla):
+    def __muertesPorMillon(self, tabla):
         muertes = tabla['Muertes']
         poblacion = tabla['Población'].mul(100000)
         resultado = muertes.div(poblacion).mul(1000000)
         tabla["Muertes*millon"] = resultado
         return tabla
 
-    def beneficio(self, tabla):
+    def __beneficio(self, tabla):
         tabla2 = tabla.sort_values('est*hab', ascending = False) 
         indices = indices = [i for i in range(1,8)]
         tabla2['Beneficio'] = indices
         return tabla2.sort_index()
 
-    def ventiladores(self, tabla):
+    def __ventiladores(self, tabla):
         tabla2 = tabla.sort_values('Muertes*millon', ascending = False) 
         indices1 = [2,2]
         indices2 = [1 for i in range(0, len(tabla2['Muertes'])-2)]
@@ -45,15 +45,53 @@ class LinearProgramming():
         tabla2['Ventiladores'] = indices
         return tabla2.sort_index()
 
-        
+    def generarTabla(self):
+        tabla = self.__construirTabla()
+        tabla = self.__estacionesPorHabitantes(tabla)
+        tabla = self.__muertesPorMillon(tabla)
+        tabla = self.__beneficio(tabla)
+        tabla = self.__ventiladores(tabla)
+        return tabla
 
-objec = LinearProgramming(4)
-table = objec.generarTabla()
-result = objec.estacionesPorHabitantes(table)
-result2 = objec.muertesPorMillon(result)
-result3 = objec.beneficio(result2)
-result4 = objec.ventiladores(result3)
-print(result4)
-
+    def __generarTablaModelo(self, tabla):
+        variables_regiones = [LpVariable(f'x{i}', lowBound=0, cat='Integer') for i in range (1, self.__num_regiones + 1)]
+        beneficio = tabla['Beneficio'].values
+        ventiladores = tabla['Ventiladores'].values
+        personal = tabla['Personal'].values
+        costos = tabla['Costos'].values
+        cualificacion = tabla['Cualificación'].values
+        info = [variables_regiones, beneficio, ventiladores, personal, costos, cualificacion]
+        tabla_modelo = pd.DataFrame(info, columns=[f'Region{i}' for i in range(1, self.__num_regiones + 1)])
+        return tabla_modelo, variables_regiones
     
+    def __variablesModelo(self, tabla):
+        tabla_modelo, variables_regiones = tabla
+        funcion_objetivo = 0
+        ventiladores = 0
+        personal = 0
+        costo = 0
+        cualificacion = 0
+        for nombre_columna, contenido in tabla_modelo.items():
+            funcion_objetivo += contenido[1]*contenido[0]
+            ventiladores += contenido[2]*contenido[0]
+            personal += contenido[3]*contenido[0]
+            costo += contenido[4]*contenido[0]
+            cualificacion += contenido[5]*contenido[0]
+        return variables_regiones, funcion_objetivo, ventiladores, personal, costo, cualificacion
+    
+    def modelo(self, tabla, total_ventiladores=1000, total_personal=2000, total_costo=3500000000, total_cualificacion=1000):
+        tabla_modelo = self.__generarTablaModelo(tabla)
+        regiones, funcion_objetivo, ventiladores, personal, costo, cualificacion = self.__variablesModelo(tabla_modelo)
+        model = LpProblem('Modelo_proyecto', LpMaximize)
+        model += funcion_objetivo
+        model += ventiladores <= total_ventiladores
+        model += personal <= total_personal
+        model += costo <= total_costo
+        model += cualificacion >= total_cualificacion
+        model.solve()
+        return value(regiones[0]), value(regiones[1]), value(regiones[2]), value(regiones[3]), value(regiones[4]), value(regiones[5]), value(regiones[6]), value(model.objective)
 
+objeto = LinearProgramming(num_regiones=7)
+tabla = objeto.generarTabla()
+resultado = objeto.modelo(tabla)
+print(resultado)
